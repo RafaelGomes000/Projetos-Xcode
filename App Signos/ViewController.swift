c46f05d52829809ee7ce549d901ee32eec599da6
Rafael Almeida <rafael.gomees00@gmail.com>
2023-01-02 11:56:33 -0300

    configurando array

--- App Signos/ViewController.swift
+++ App Signos/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UITableViewController {
     
     var signos: [String] = []
+    var significadoSignos: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,21 @@ class ViewController: UITableViewController {
         signos.append("Capricornio")
         signos.append("Aquario")
         signos.append("Peixes")
+        
+        
+        //Configurar significado
+        significadoSignos.append("O ariano...")
+        significadoSignos.append("Touro...")
+        signos.append("3")
+        signos.append("4")
+        signos.append("5")
+        signos.append("6")
+        signos.append("7")
+        signos.append("8")
+        signos.append("9")
+        signos.append("10")
+        signos.append("11")
+        
         
     }
 
