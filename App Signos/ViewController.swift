1addd39c3bdad807c574b6ed51d459e9ce92fe31
Rafael Almeida <rafael.gomees00@gmail.com>
2023-01-02 16:53:52 -0300

    Configurando frases/Configurando Alert

--- App Signos/ViewController.swift
+++ App Signos/ViewController.swift
@@ -10,42 +10,43 @@ import UIKit
 class ViewController: UITableViewController {
     
     var signos: [String] = []
-    var significadoSignos: [String] = []
+    var dataSignos: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Configurar signos
-        signos.append("Aries")
+        //configuração signos
+        signos.append("Áries")
         signos.append("Touro")
-        signos.append("Gemeos")
-        signos.append("Cancer")
-        signos.append("Leao")
+        signos.append("Gêmeos")
+        signos.append("Câncer")
+        signos.append("Leão")
         signos.append("Virgem")
         signos.append("Libra")
-        signos.append("Escorpiao")
-        signos.append("Sargitario")
-        signos.append("Capricornio")
-        signos.append("Aquario")
+        signos.append("Escorpião")
+        signos.append("Sagitário")
+        signos.append("Capricórnio")
+        signos.append("Aquário")
         signos.append("Peixes")
         
         
-        //Configurar significado
-        significadoSignos.append("O ariano...")
-        significadoSignos.append("Touro...")
-        significadoSignos.append("3")
-        significadoSignos.append("4")
-        significadoSignos.append("5")
-        significadoSignos.append("6")
-        significadoSignos.append("7")
-        significadoSignos.append("8")
-        significadoSignos.append("9")
-        significadoSignos.append("10")
-        significadoSignos.append("11")
-        significadoSignos.append("12")
+        //configuração mês
+        dataSignos.append("21 de março a 20 de abril")
+        dataSignos.append("21 de abril a 20 de maio")
+        dataSignos.append("21 de maio a 20 de junho")
+        dataSignos.append("21 de junho a 22 de julho")
+        dataSignos.append("23 de julho a 22 de agosto")
+        dataSignos.append("23 de agosto a 22 de setembro")
+        dataSignos.append("23 de setembro a 22 de outubro")
+        dataSignos.append("23 de outubro a 21 de novembro")
+        dataSignos.append("22 de novembro a 21 de dezembro")
+        dataSignos.append("22 de dezembro a 20 de janeiro")
+        dataSignos.append("21 de janeiro a 19 de fevereiro")
+        dataSignos.append("19 de fevereiro a 20 de março")
         
     }
 
+    //configuração TableView
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
@@ -61,6 +62,21 @@ class ViewController: UITableViewController {
         celula.textLabel?.text = signos[indexPath.row]
         
         return celula
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //configuração AlertController
+        let alertaController = UIAlertController(title: "Data do signo", message: dataSignos [indexPath.row], preferredStyle: .alert)
+        
+        let acaoConfirmar = UIAlertAction(title: "ok", style: .default)
+        
+        alertaController.addAction(acaoConfirmar)
+        
+        present(alertaController, animated: true)
+        
+        
     }
     
 }
