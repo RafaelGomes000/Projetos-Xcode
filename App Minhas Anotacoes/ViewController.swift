32eede22af8cde3a290c78f19d34785509214424
Rafael Almeida <rafael.gomees00@gmail.com>
2023-01-03 11:25:59 -0300

    Configurando UserDefault

--- App Minhas Anotacoes/ViewController.swift
+++ App Minhas Anotacoes/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Configurando App
+    @IBOutlet weak var textoCampo: UITextView!
+    let chave = "minhaAnotacao"
+    
+    @IBAction func salvarAnotacao(_ sender: Any) {
+        
+        //Guardando dados
+        if let text = textoCampo.text{
+            UserDefaults.standard.set(text, forKey: chave)
+        }
+    }
+    
+    func recuperarAnotacao() -> String{
+        
+        if let textoRecuperado = UserDefaults.standard.object(forKey: chave){
+            return textoRecuperado as! String
+        }
+            
+        return ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textoCampo.text = recuperarAnotacao()
     }
 
 
