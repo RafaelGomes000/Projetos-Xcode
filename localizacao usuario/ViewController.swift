4a3ff21de4f420e6b5232ae4dad04929fcee5348
Rafael Almeida <rafael.gomees00@gmail.com>
2023-01-04 17:05:42 -0300

    Configurações principais

--- localizacao usuario/ViewController.swift
+++ localizacao usuario/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    var gerenciadorLocal = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        gerenciadorLocal.delegate = self
+        //presicao de localizacao
+        gerenciadorLocal.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //solicitacao de localizacao do usuario
+        gerenciadorLocal.requestWhenInUseAuthorization()
+        
+        //inicio de atualizacao da localizacao do usuario
+        gerenciadorLocal.startUpdatingLocation()
     }
 
 
