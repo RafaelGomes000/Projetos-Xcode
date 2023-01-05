f924741348214a2e5da90e06bc32fdf4c404c17e
Rafael Almeida <rafael.gomees00@gmail.com>
2023-01-05 11:24:09 -0300

    Configurando localização

--- App Onde estou/ViewController.swift
+++ App Onde estou/ViewController.swift
@@ -6,12 +6,66 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapa: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
+    
+    
+    @IBOutlet weak var velocidadeLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var enderecoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+    }
+    
+    //passando localizacao
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var localizacaoUsuario = locations.last!
+        
+        let longitude = localizacaoUsuario.coordinate.longitude
+        let latitude = localizacaoUsuario.coordinate.latitude
+        
+        longitudeLabel.text = String(longitude)
+        latitudeLabel.text = String(latitude)
+        
+        velocidadeLabel.text = String(localizacaoUsuario.speed)
+    }
+    
+    //Alerta para autorizar localizaçao
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status != CLAuthorizationStatus.authorizedWhenInUse{
+            
+            //mensagem alerta
+            var alertaController = UIAlertController(title: "Permissao de localizaçap", message: "Necessário parmissao para acesso a sua localizaçao. Por favor habilite.", preferredStyle: .alert)
+            
+            //opcao configurar
+            var acaoConfigurar = UIAlertAction(title: "Abrir configuraçao", style: .default, handler: {(alertaConfiguracoes) in
+                
+                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString){
+                    UIApplication.shared.open(configuracoes as URL)
+                }
+                
+            })
+            
+            //opcao cancelar
+            var acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            
+            alertaController.addAction(acaoConfigurar)
+            alertaController.addAction(acaoCancelar)
+            
+            present(alertaController, animated: true)
+        }
     }
 
 
