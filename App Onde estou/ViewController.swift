23ce0ad2b87a96cad09cc673084ca1562767b5f5
Rafael Almeida <rafael.gomees00@gmail.com>
2023-01-05 12:37:55 -0300

    Adicionando atualização nome rua em tempo real

--- App Onde estou/ViewController.swift
+++ App Onde estou/ViewController.swift
@@ -1,23 +1,24 @@
 //
 //  ViewController.swift
-//  App Onde estou
+//  Onde estou Aula
 //
-//  Created by teste on 05/01/23.
+//  Created by Jamilton  Damasceno
+//  Copyright © Jamilton  Damasceno. All rights reserved.
 //
 
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate  {
+    
     @IBOutlet weak var mapa: MKMapView!
     var gerenciadorLocalizacao = CLLocationManager()
     
-    
     @IBOutlet weak var velocidadeLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
-    @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var enderecoLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,81 +27,144 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest
         gerenciadorLocalizacao.requestWhenInUseAuthorization()
         gerenciadorLocalizacao.startUpdatingLocation()
+        
     }
     
-    //passando localizacao
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         let localizacaoUsuario = locations.last!
         
         let longitude = localizacaoUsuario.coordinate.longitude
         let latitude = localizacaoUsuario.coordinate.latitude
         
-        let deltaLatitude: CLLocationDegrees = 0.003
-        let deltaLongitude: CLLocationDegrees = 0.003
+        self.longitudeLabel.text = String(longitude)
+        self.latitudeLabel.text = String(latitude)
         
-        longitudeLabel.text = String(longitude)
-        latitudeLabel.text = String(latitude)
+        if localizacaoUsuario.speed > 0 {
+            velocidadeLabel.text = String( localizacaoUsuario.speed )
+        }
         
-        velocidadeLabel.text = String(localizacaoUsuario.speed)
+        let deltaLat: CLLocationDegrees = 0.005
+        let deltaLon: CLLocationDegrees = 0.005
         
-        
-        let areaAproximacao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude, longitudeDelta: deltaLongitude)
-        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let regiao: MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaAproximacao)
-        
-        //mostrando localizacao na tela
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let areaExibicao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLat, longitudeDelta: deltaLon)
+        let regiao: MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaExibicao)
         mapa.setRegion(regiao, animated: true)
         
-        //endereco
-        CLGeocoder().reverseGeocodeLocation(localizacaoUsuario) { (detalhesLocal, erro) in
+        CLGeocoder().reverseGeocodeLocation( localizacaoUsuario) { (detalhesLocal, erro) in
             
-            if erro == nil{
+            if erro == nil {
                 
-                if let dadosLocal = detalhesLocal?.first{
+                if let dadosLocal = detalhesLocal?.first {
+                
+                    //rua
                     var thoroughfare = ""
-                    if dadosLocal.thoroughfare != nil{
-                    thoroughfare = dadosLocal.thoroughfare!
-                        
-                    var subThoroughfare = ""
-                    if dadosLocal.subThoroughfare != nil{
-                    thoroughfare = dadosLocal.subThoroughfare!
+                    if dadosLocal.thoroughfare != nil {
+                        thoroughfare = dadosLocal.thoroughfare!
                     }
                     
-                }else{
+                    //numero
+                    var subThoroughfare = ""
+                    if dadosLocal.subThoroughfare != nil {
+                        subThoroughfare = dadosLocal.subThoroughfare!
+                    }
                     
-                    print(erro)
+                    //cidade
+                    var locality = ""
+                    if dadosLocal.locality != nil {
+                        locality = dadosLocal.locality!
+                    }
                     
+                    //bairro
+                    var subLocality = ""
+                    if dadosLocal.subLocality != nil {
+                        subLocality = dadosLocal.subLocality!
+                    }
+                    
+                    //cep
+                    var postalCode = ""
+                    if dadosLocal.postalCode != nil {
+                        postalCode = dadosLocal.postalCode!
+                    }
+                    
+                    //pais
+                    var country = ""
+                    if dadosLocal.country != nil {
+                        country = dadosLocal.country!
+                    }
+                    
+                    var administrativeArea = ""
+                    if dadosLocal.administrativeArea != nil {
+                        administrativeArea = dadosLocal.administrativeArea!
+                    }
+                    
+                    var subAdministrativeArea = ""
+                    if dadosLocal.subAdministrativeArea != nil {
+                        subAdministrativeArea = dadosLocal.subAdministrativeArea!
+                    }
+                    
+                    self.enderecoLabel.text = thoroughfare + " - "
+                                              + subThoroughfare + " / "
+                                              + locality + " / "
+                                              + country
+                    
+                    print(
+                            "\n / thoroughfare:" + thoroughfare +
+                            "\n / subThoroughfare:" + subThoroughfare +
+                            "\n / locality:" + locality +
+                            "\n / subLocality:" + subLocality +
+                            "\n / postalCode:" + postalCode +
+                            "\n / country:" + country +
+                            "\n / administrativeArea:" + administrativeArea +
+                            "\n / subAdministrativeArea:" + subAdministrativeArea
+                    )
+                    
+                
                 }
                 
+            }else{
+                print(erro ?? "")
             }
+            
         }
+
+        
     }
     
-    //Alerta para autorizar localizaçao
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         
-        if status != CLAuthorizationStatus.authorizedWhenInUse{
+        if status != .authorizedWhenInUse {
             
-            //mensagem alerta
-            let alertaController = UIAlertController(title: "Permissao de localizaçap", message: "Necessário parmissao para acesso a sua localizaçao. Por favor habilite.", preferredStyle: .alert)
+            let alertaController = UIAlertController(title: "Permissão de localização",
+                                                     message: "Necessário permissão para acesso à sua localização!! por favor habilite.",
+                                                     preferredStyle: .alert )
             
-            //opcao configurar
-            let acaoConfigurar = UIAlertAction(title: "Abrir configuraçao", style: .default, handler: {(alertaConfiguracoes) in
+            let acaoConfiguracoes = UIAlertAction(title: "Abrir configurações", style: .default , handler: { (alertaConfiguracoes) in
                 
-                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString){
-                    UIApplication.shared.open(configuracoes as URL)
+                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString ) {
+                    UIApplication.shared.open( configuracoes as URL )
                 }
                 
             })
+
+            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .default , handler: nil )
             
-            //opcao cancelar
-            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            alertaController.addAction( acaoConfiguracoes )
+            alertaController.addAction( acaoCancelar )
             
-            alertaController.addAction(acaoConfigurar)
-            alertaController.addAction(acaoCancelar)
+            present( alertaController , animated: true, completion: nil )
             
-            present(alertaController, animated: true)
+            
         }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
